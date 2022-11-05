212640b5dd19a14d6410a53eb1a8b319d9afa54d
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-11-05 09:39:51 +0800

    Release 3.4

--- patch_server_ip.sh
+++ patch_server_ip.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sha512sum "$1"
 echo "Patching server IP for $1"
-sed -i "s/209.222.97.175/enginetribe.gq/g" "$1"
-sha512sum "$1"
+sed -i "s/209.222.97.175:25019/juego.enginetribe.gq/g" "$1"
